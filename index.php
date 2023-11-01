0531ef39ec775f546c5387165ffc667a6524b1a6
Svavushin <shavushin@gmail.com>
2023-11-01 17:53:19 +0900

    Replace index.html to index.php. Delete unnecessory php file

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+include 'php/test_.php'
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>TEST</title>
+  <link href="/css/styles.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+  <div class="block">
+
+    <div>
+      111111
+    </div>
+
+    <div>
+      111111
+    </div>
+
+    <div>
+      111111
+    </div>
+
+    <div>
+      111111
+    </div>
+
+    <div>
+      111111
+    </div>
+
+  </div>
+
+  <div class="block-ul">
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
+  </div>
+
+  </div>
+
+  <div class="block-form">
+    <form action="" style="display: block;">
+
+      <input type="text" placeholder="num"> <br>
+
+      <input type="text" placeholder="string"> <br>
+
+      <input type="file" placeholder="file"> <br>
+
+
+    </form>
+  </div>
+
+
+</body>
+
+</html>
