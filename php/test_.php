cbd2e047466e3634a4ed9940a8e5465653cc5a83
Svavushin <shavushin@gmail.com>
2023-11-02 12:01:10 +0900

    somithing

--- php/test_.php
+++ php/test_.php
@@ -1,1 +1,15 @@
 <?php
+echo '<pre>';
+var_dump($_FILES['file1']);
+var_dump($_POST['num']);
+var_dump($_POST['string']);
+echo '</pre>';
+
+
+$files = "./files/" . $_FILES['file1']['name'];
+
+var_dump(move_uploaded_file($_FILES['file1']['tmp_name'], $files));
+
+if (move_uploaded_file($_FILES['file1']['tmp_name'], $files)) {
+  echo $_FILES['file1']['name'] . "успешно загружен";
+}
